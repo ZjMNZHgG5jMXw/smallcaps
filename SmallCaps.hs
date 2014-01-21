77c50b89e5a0767ecaaabca10f58090950846237
Stefan Berthold <stefan.berthold@gmx.net>
2014-01-21 17:48:21 +0100

    removed IO.

--- SmallCaps.hs
+++ SmallCaps.hs
@@ -1,24 +1,18 @@
 module SmallCaps where
 
-import Prelude hiding ( getContents, putStr )
-
 import Data.Text            ( Text )
-import Data.Text.IO         ( getContents, putStr )
 import Data.Attoparsec.Text ( parseOnly )
 import Text.Parsec          ( parse )
 
-import Data.LaTeX
-import Data.Config
-import Text.TeXParser
-import Text.TeXLaTeXParser
-import Text.DocumentParser
+import Data.LaTeX           ( unlatex )
+import Data.Config          ( Config )
+import Text.TeXParser       ( tex )
+import Text.TeXLaTeXParser  ( latex )
+import Text.DocumentParser  ( runDocument )
 
 smallcaps :: Config -> Text -> Text
 smallcaps conf = unlatex . runDocument conf . parseLaTeX . parseTeX where
   parseTeX = either error id . parseOnly tex
   parseLaTeX = either (error . show) id . parse latex ""
-
-smallcapsPipe :: Config -> IO ()
-smallcapsPipe conf = putStr . smallcaps conf =<< getContents
 
 -- vim: ft=haskell:sts=2:sw=2:et:nu:ai
