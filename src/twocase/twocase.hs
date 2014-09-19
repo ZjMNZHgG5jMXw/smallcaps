59b191b7f0931b06491895d63ff9cd357264c355
Stefan Berthold <stefan.berthold@gmx.net>
2014-09-19 19:58:38 +0200

    added twocase.

---
+++ src/twocase/twocase.hs
@@ -0,0 +1,46 @@
+-------------------------------------------------------------------------------
+---- |
+---- Module      :  Main
+---- Copyright   :  (c) Stefan Berthold 2014
+---- License     :  BSD3-style (see LICENSE)
+----
+---- Maintainer  :  stefan.berthold@gmx.net
+---- Stability   :  unstable
+---- Portability :  
+----
+---- Alternative implementation for the command line interface of SmallCaps.
+----
+-------------------------------------------------------------------------------
+
+module Main where
+
+import Data.Default           ( def )
+import Data.Map               ( insert )
+import System.Environment     ( withProgName )
+import Data.Text as T         ( Text, pack, length )
+import Text.SmallCaps         ( smallcaps )
+import Text.SmallCaps.Config  ( Config ( replaceFilter ), Profile, defaultProfile, small, footnote )
+
+main :: IO ()
+main  = withProgName "twocase"
+      $ smallcaps twoConf twoProfile
+
+twoFilter :: Text -> Bool
+twoFilter = (<) 1 . T.length
+
+twoConf :: Config
+twoConf = def { replaceFilter = twoFilter }
+
+twoSmall :: Config
+twoSmall = small { replaceFilter = twoFilter }
+
+twoFootnote :: Config
+twoFootnote = footnote { replaceFilter = twoFilter }
+
+twoProfile :: Profile
+twoProfile  = insert (pack "default")   twoConf
+            $ insert (pack "small")     twoSmall
+            $ insert (pack "footnote")  twoFootnote
+            $ defaultProfile
+
+-- vim: ft=haskell:sts=2:sw=2:et:nu:ai
